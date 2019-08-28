e13e1e3b92673f61a8631db50429d096049b9d1f
Braidon Lee <braidonl@hotmail.com>
2019-08-28 15:29:52 -0300

    1st day of Laravel From Scratch

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layout')
+
+@section('content')
+    <h1>Edit Project</h1>
+
+    <form method="POST" action="/projects/{{ $project->id }}" style="margin-bottom: 1em;">
+        {{ method_field('PATCH') }}
+        {{ csrf_field() }}
+
+        <div class="field">
+            <label for="title" class="label">Title</label>
+            <div class="control">
+                <input type="text" name="title" class="input" placeholder="Title" value="{{ $project->title }}">
+            </div>
+        </div>
+
+        <div class="field">
+            <label for="description" class="label">Description</label>
+            <div class="control">
+                <textarea name="description" class="textarea">{{ $project->description }}</textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Update Project</button>
+            </div>
+        </div>
+    </form>
+
+    <form method="POST" action="/projects/{{ $project->id }}">
+        {{ method_field('DELETE') }}
+        {{ csrf_field() }}
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-danger">Delete Project</button>
+            </div>
+        </div>
+    </form>
+@endsection
